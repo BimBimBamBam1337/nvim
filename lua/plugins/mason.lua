348f8d62cb9ed713d143931526ef7efa18730d69
BimBimBamBam1337 <a94440003@gmail.com>
2025-12-05 10:35:52 +0300

    feat: update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,34 +1,4 @@
 return {
-    -- Mason для управления LSP
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            -- Инициализация Mason
-            require("mason").setup()
-        end,
-    },
-
-    -- --Mason + LSP
-    -- {
-    --     "williamboman/mason-lspconfig.nvim",
-    --     dependencies = { "williamboman/mason.nvim" },
-    --     config = function()
-    --         -- Автоматическая установка и настройка LSP через Mason
-    --         require("mason-lspconfig").setup({
-    --             ensure_installed = {
-    --                 "pyright",  -- LSP для Python
-    --                 "dockerls", -- LSP для Docker
-    --                 "jsonls",   -- LSP для JSON
-    --                 "lua_ls",   -- LSP для Lua
-    --                 "yamlls",   -- LSP для YAML
-    --                 "marksman", -- LSP для Markdown
-    --             },
-    --         })
-    --     end,
-    -- },
-
-    -- Конфигурация LSP
-    {
     "neovim/nvim-lspconfig",
     dependencies = {
         "williamboman/mason.nvim",
@@ -37,8 +7,6 @@ return {
     },
     config = function()
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-
-        -- Capabilities for autocompletion
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
         require("mason").setup()
@@ -52,19 +20,13 @@ return {
                 "yamlls",
                 "marksman",
             },
-
-            -- Новый API вместо setup_handlers
             handlers = {
                 function(server)
-                    vim.lsp.config[server].setup({
+                    require("lspconfig")[server].setup({
                         capabilities = capabilities,
                     })
                 end,
             },
-            })
-        end,
-    }
-
-
+        })
+    end,
 }
-
