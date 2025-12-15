c51631dc2292f8f43f467a5f167bd7d65beb1bf2
BimBimBamBam1337 <a94440003@gmail.com>
2025-12-15 22:11:46 +0300

    feat: fix error

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,38 +1,34 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-    -- Подключаем нужные источники: форматтеры, линтеры, кодовые действия и т.д.
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.prettier,      -- для JS, TS, HTML, CSS и т.д.
-        null_ls.builtins.formatting.black,         -- для Python
-        null_ls.builtins.formatting.stylua,        -- для Lua
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.stylua,
 
-        null_ls.builtins.diagnostics.eslint,       -- для JS/TS
-        null_ls.builtins.diagnostics.flake8,       -- для Python
+                null_ls.builtins.diagnostics.eslint,
+                --null_ls.builtins.diagnostics.ruff,
 
-        null_ls.builtins.code_actions.gitsigns,    -- действия по git
-
-        -- null_ls.builtins.formatting.your_custom_tool.with({ ... })
-      },
-      -- Настройки LSP-клиента none-ls
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = vim.api.nvim_create_augroup("Format", { clear = true }),
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
+                null_ls.builtins.code_actions.gitsigns,
+            },
+            on_attach = function(client, bufnr)
+                -- Автоформатирование
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = vim.api.nvim_create_augroup("Format", { clear = true }),
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
             end,
-          })
-        end
-      end,
-    })
-  end,
+        })
+    end,
 }
-
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,17 +1,16 @@
-
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  opts = {
-    bigfile = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    picker = { enabled = true },
-    notify = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-  },
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        bigfile = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true },
+        notify = { enabled = false },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+    },
 }
